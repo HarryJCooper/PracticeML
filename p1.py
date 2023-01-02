469aeb75a4f41f712788dc2540b5c87a04bf388d
DA-HarryCooper <harry@purplejamaudio.com>
2023-01-02 10:40:09 +0000

    first commit

---
+++ p1.py
@@ -0,0 +1,28 @@
+# THIS IS A SINGLE NEURON, EVERY INPUT HAS A WEIGHT, BUT ONLY ONE BIAS/NEURON
+
+# THE INPUT LAYER IS JUST THE VALUES YOU PUT IN
+
+# THE OUTPUT NEURONS 
+
+# IN MATHEMATICS A LIST IS A VECTOR
+# A LIST OF LISTS IS A 2D ARRAY
+# A MATRIX IS JUST A RECTANGULAR ARRAY
+# A LIST OF VECTORS IS A MATRIX
+# A LIST OF LISTS OF LISTS IS A 3D ARRAY
+# A TENSOR IS AN OBJECT THAT CAN BE REPRESENTED AS AN ARRAY
+
+# the dot_product e.g a . b = [1, 2, 3] . [2, 3, 4] = 1 * 2 + 2 * 3 + 3 * 4 = 20
+# the dot_product of two vectors (lists) results in a single scalar value
+# a scaler value is a value that only has one component to it
+
+
+
+inputs = [1, 2, 3, 2.5]
+weights = [0.2, 0.8, -0.5, 1.0]
+bias = 2
+# weights = [[0.2, 0.8, -0.5, 1.0],
+#         [0.5, -0.91, 0.26, -0.5],
+#         [-0.26, -0.27, 0.17, 0.87]]
+
+# biases = [2, 3, 0.5]
+
