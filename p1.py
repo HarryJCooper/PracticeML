0d0e894b6b940a5a529956ec0babb31efb2bd8b2
DA-HarryCooper <harry@purplejamaudio.com>
2023-01-02 12:53:52 +0000

    extrapolate to class

--- p1.py
+++ p1.py
@@ -15,14 +15,45 @@
 # the dot_product of two vectors (lists) results in a single scalar value
 # a scaler value is a value that only has one component to it
 
+import numpy as np
+np.random.seed(0)
+# WANT A TO PASS A BATCH OF INPUT SAMPLES
+X = [[1, 2, 3, 2.5],
+    [2.0, 5.0, -1.0, 2.0],
+    [-1.5, 2.7, 3.3, -0.8]]
 
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.1 * np.random.randn(n_inputs, n_neurons) # PARAMETERS ARE THE SHAPE
+        self.biases = np.zeros((1, n_neurons))
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
 
-inputs = [1, 2, 3, 2.5]
-weights = [0.2, 0.8, -0.5, 1.0]
-bias = 2
+layer1 = Layer_Dense(4, 5)
+layer2 = Layer_Dense(5, 2)
+
+layer1.forward(X)
+print(layer1.output)
+layer2.forward(layer1.output)
+print(layer2.output)
+
 # weights = [[0.2, 0.8, -0.5, 1.0],
 #         [0.5, -0.91, 0.26, -0.5],
 #         [-0.26, -0.27, 0.17, 0.87]]
 
 # biases = [2, 3, 0.5]
 
+# weights2 = [[0.1, -0.14, 0.5],
+#         [-0.5, 0.12, -0.33],
+#         [-0.44, 0.73, -0.13]]
+
+# biases2 = [-1, 2, -0.5]
+
+# # THE FIRST ELEMENT YOU PASS THE np.dot DETERMINES THE OUTPUT
+# # THE FIRST ELEMENT CAN BE A MATRIX (A LIST/ARRAY OF VECTORS)
+# # THEN IT ITERATES THROUGH EACH VECTOR AND GETS THE DOT PRODUCT AGAINST THE INPUT
+
+# layer1_outputs = np.dot(inputs, np.array(weights).T) + biases
+# layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
+
+# print(layer2_outputs)
