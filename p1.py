b8be52bb7c7c69052927dd885ea7c8bb5286cef5
DA-HarryCooper <harry.cooper@dawsonandrews.com>
2023-01-02 18:45:25 +0000

    wip

--- p1.py
+++ p1.py
@@ -15,12 +15,25 @@
 # the dot_product of two vectors (lists) results in a single scalar value
 # a scaler value is a value that only has one component to it
 
+# ACTIVATION FUNCTION IS REQUIRED TO STOP IT ALL BEING LINEAR, ACTIVATION REFERS TO A CERTAIN CHARGE BEING NEEDED TO ACTIVATE A SYNAPSE
+
 import numpy as np
+import nnfs
+
+nnfs.init()
+
 np.random.seed(0)
 # WANT A TO PASS A BATCH OF INPUT SAMPLES
 X = [[1, 2, 3, 2.5],
     [2.0, 5.0, -1.0, 2.0],
     [-1.5, 2.7, 3.3, -0.8]]
+
+inputs = [0, 2, -1, 3.3, -2.7, 1.1, 2.2, -100]
+output = []
+
+for i in inputs:
+    output.append(max(0, 1))
+    
 
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
@@ -28,6 +41,11 @@ class Layer_Dense:
         self.biases = np.zeros((1, n_neurons))
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
+
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
 
 layer1 = Layer_Dense(4, 5)
 layer2 = Layer_Dense(5, 2)
